0c1b00f613d2e37174d5dc9650a2bac89c33554a
Jamerze <70536433+Jamerze@users.noreply.github.com>
2022-03-10 13:39:25 +0100

    Add footer
    Footer added and transformed the code to React syntax. Order of CSS file is not optimal in this commit.

--- Frontend/FRONTEND.md
+++ Frontend/FRONTEND.md
@@ -1,2 +1,4 @@
 # Frontend
 The frontend of this platform will be build with ReactJS.
+
+Note 08-03 - Change css order (as in the original)
--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Signup } from './Components/Signup';
 
 function App() {
   return (
     <div className="App">
+      <Signup />
     </div>
   );
 }
