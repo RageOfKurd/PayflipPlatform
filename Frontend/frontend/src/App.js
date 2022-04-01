332f93a8e2eae96df846f09b804cb3625d794812
unknown <nvar.313185196@uhd.edu.iq>
2022-04-01 17:29:23 +0300

    added dashboard for (admin, employee,employer)

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import SignUp from './components/sign-up';
 import SignIn from './components/sign-in';
 import './App.css';
+import { AdminDash } from './components/admin-dashboard';
+import { EmployerDash } from './components/employer-dashboard';
+import { EmployeeDash } from './components/employee-dashboard';
 
 function App() {
   return (
-<SignIn />
+<EmployeeDash/>
  
   );
 }
